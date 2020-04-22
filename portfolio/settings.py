a3ffaf49250c496a44bac5e7075ffeec808fb261
Leslie Caminade <lesliecaminade@gmail.com>
2020-04-22 18:28:15 +0800

    added locally hosted bootstrap

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICSFILES_DIRS = [
-    STATIC_ROOT,
+STATICFILES_DIRS = [
+    STATIC_DIR, 
 ]
 
 MEDIA_URL = '/media/'
