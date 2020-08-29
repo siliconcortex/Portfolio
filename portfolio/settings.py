bb435671fa554d28bddeca12489d945bd53f6ad5
Leslie Caminade <lesliecaminade@gmail.com>
2020-08-29 13:03:31 +0800

    feature: new site layout copying henrypp website

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -83,20 +83,20 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# DATABASES = { 'default': {
-#     'ENGINE': 'django.db.backends.mysql',
-#     'NAME': 'lesliecaminade$default',
-#     'USER': 'lesliecaminade',
-#     'PASSWORD': 'testpasswordsql',
-#     'HOST': 'lesliecaminade.mysql.pythonanywhere-services.com', }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = { 'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'lesliecaminade$default',
+    'USER': 'lesliecaminade',
+    'PASSWORD': 'testpasswordsql',
+    'HOST': 'lesliecaminade.mysql.pythonanywhere-services.com', }
+}
 
 
 # Password validation
